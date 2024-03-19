f7f69340c96b4bb9a6707921ba31d54621a0ea90
MaryIawq <mary.iawq@gmail.com>
2024-03-19 14:17:29 +0900

    feat: fox movement

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <script setup>
-import {ref} from "vue";
+import {onMounted, onUnmounted, ref, watch} from "vue";
 
 import BackgroundItemsComponent from "@/components/background-items-component.vue";
 import ScoreComponent from "@/components/score-component.vue";
 import HouseComponent from "@/components/house-components/house-component.vue";
 import FloorComponent from "@/components/floor-component.vue";
 
-const score = ref(0)
+const fox = {
+  position: ref(0),
+  lookingLeft: ref(false),
+  width: 150
+}
+
+const handleKeyPress = (event) => {
+  if (event.code === 'ArrowLeft') {
+    if (fox.position.value > 0) {
+      fox.position.value -= 15;
+      fox.lookingLeft.value = true;
+    }
+  } else if (event.code === 'ArrowRight') {
+    if (fox.position.value + fox.width < window.innerWidth - 50) {
+      fox.position.value += 15;
+      fox.lookingLeft.value = false
+    }
+  }
+}
+
+
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress)
+})
 </script>
 
 <template>
@@ -17,7 +41,7 @@ const score = ref(0)
       <div class="items-container">
         <img class="item" src="">
       </div>
-      <img class="fox" src="/fox.png" alt="game-character">
+      <img class="fox" src="/fox.png" alt="game-character" :style="{ left: `${fox.position.value}px`, transform: fox.lookingLeft.value ? 'scaleX(-1)' : 'scaleX(1)' }">
       <house-component></house-component>
       <floor-component></floor-component>
     </div>
